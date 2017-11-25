14c8997bfccacaa6ff13714920da260c35d3dd99
rishabhiitbhu <rishabh.agrahari.eee15@iitbhu.ac.in>
2017-11-25 18:57:51 +0530

    switched to django 1.11, static serving done

--- hk/web/web/urls.py
+++ hk/web/web/urls.py
@@ -1,25 +1,26 @@
-from django.conf import settings
-from django.conf.urls import patterns, include, url
-from django.conf.urls.static import static
+"""web URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
 from django.contrib import admin
-admin.autodiscover()
+from aqp import views
 
-urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'aqp.views.home', name='home'),
-    url(r'^about', 'aqp.views.about', name='about'),
-    url(r'^contact', 'aqp.views.contact', name='contact'),
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home, name='home'),
+    url(r'^about', views.about, name='about'),
+    url(r'^contact', views.contact, name='contact'),
 
-
-
-    # url(r'^blog/', include('blog.urls')),
-    #(?P<all_items>.*)
-    #(?P<id>\d+)
-    url(r'^admin/', include(admin.site.urls)),
-
-)
-
-
-if settings.DEBUG:
-	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
