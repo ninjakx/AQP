55ec54e1b71e9293ed21a7b837fa39213bd0a33e
ninjakx <samurai.kittycat@gmail.com>
2018-01-02 14:51:03 +0530

    changes

--- hk/web/aqp/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class ContactForm(forms.Form):
-    cname = forms.CharField(widget=forms.TextInput(attrs={'type':'text','class':'form-control' ,'placeholder':'Name' }))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'type':'text','class':'form-control','placeholder':'Email'}))
-    msg = forms.CharField(widget=forms.Textarea(attrs={'type':'text','class':'form-control','maxlength':150,'placeholder':'Message'}))
--- hk/web/aqp/models.py
+++
@@ -1,6 +0,0 @@
-from django.core.urlresolvers import reverse
-from django.db import models
-from django.db.models.signals import post_save
-
-from django.conf import settings
-
