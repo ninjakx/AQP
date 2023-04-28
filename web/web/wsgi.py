60227c7fc5fbf37762a82a3cef0955d3aae49f45
Kriti Sahu <ninjakx@Kritis-MacBook-Pro.local>
2023-04-28 12:31:57 +0530

    fix: 🐛 Fixed issues

--- web/web/wsgi.py
+++ web/web/wsgi.py
@@ -7,10 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# print("basdir",BASE_DIR)
+sys.path.append(BASE_DIR+"/web")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
 
 application = get_wsgi_application()
